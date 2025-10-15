fe3c87465791ce68a9b69611d24c2d8b3d14e659
Jaritz,Markus <Markus.Jaritz@imp.ac.at>
2025-10-15 10:25:37 +0200

    added comman line arguments

--- count_beds.sh
+++ count_beds.sh
@@ -1,28 +1,77 @@
-#!/bin/bash
-set -o nounset  
-set -o errexit  
-set -o errtrace 
+#!/usr/bin/env bash
 
+set -u
+set -e
+
+# @description: Run snakemake workflow to count reads in bed files
+# @ params c configFile = YAML config file
+# @ params a smk_args = additional snakemake arguments, e.g. --dry-run, --forceall
+# @ params p processes = number of processes/cores to use (local)
+# @ params l local = run locally (no cluster)
+# @ params h help = print this help message
+
+function print_usage() {
+	echo "Usage: $0 [options]"
+	echo "Options:"
+	echo "  -c, --config FILE     Specify the configuration file in YAML format (default: count_beds.yaml)"
+	echo "  -a, --smk_args ARGS   Additional snakemake arguments (default: ''),  --dry-run, --forceall, etc."
+	echo "  -p, --processes N     Number of processes/cores to use (default: 4)"
+	echo "  -l, --local           Run locally without cluster support"
+	echo "  -h, --help            Show this help message and exit"
+	echo ""
+
+	exit 1
+}
+
+
+######
+# MAIN
+######
+
+if [[ $# == 0 ]]; then
+    print_usage
+    exit 1        # Exit and explain usage, if no argument(s) given.
+fi
+
+while getopts 'c:a:p:hl' option; do
+	case $option in
+		c ) config=$OPTARG ;;
+		a ) smk_args=$OPTARG ;;
+		p ) processes=$OPTARG ;;
+		l ) local=1 ;;
+		h ) print_usage ;;
+		* ) echo -e "\nERROR: Unimplemented option chosen!\n"
+			print_usage;;
+	esac
+done
+
+# set defaults
+configFile=${config:=count_beds.yaml}
+smk_args=${smk_args:=""}
+processes=${processes:=4}
+
+args="--configfile $config --cores $processes --show-failed-logs --snakefile /groups/gaidt/bioinf/software/scripts/deseq_pw/count_beds.smk --rerun-triggers mtime --rerun-incomplete $smk_args"
+
+# print set parameters
+>&2 echo "-c configFile=$configFile"
+>&2 echo "-a smk_args=$smk_args"
+>&2 echo "-p processes=$processes"
+if [ -z ${local+x} ]; then
+	>&2 echo "-l local=0"
+	cluster_args="--executor slurm --default-resources --cluster-generic-submit-cmd sbatch" 
+else
+	>&2 echo "-l local=1"
+	cluster_args=""
+fi
+
+
+echo "loading modules ..."
 module load build-env/f2022
 module load snakemake/9.5.1-foss-2023b
+echo "loading modules done."
 
-args="--configfile count_beds.yaml --cores 4 --show-failed-logs --snakefile ./count_beds.smk --rerun-triggers mtime --rerun-incomplete"
-cluster_args="--executor slurm --default-resources --cluster-generic-submit-cmd sbatch" 
-
-# -R [rule]:  forced restart from certain rule
-# -n : no run, only print what would be done
-# --forceall :  force all rules to be executed, even if the output files are present and up to date
-#
-# no cluster
-#
-#cmd="snakemake -n --jobs 42 $args" # -R merge_beds" #  -R featurecount " #--allowed-rules concat_featurecount --keep-incomplete --keep-going
-#cmd="snakemake --jobs 42 $args" # -R merge_beds" # featurecount " #--allowed-rules concat_featurecount all --keep-incomplete --keep-going"
-
-#
-# cluster
-#
-#cmd="snakemake -n --jobs 42 $args $cluster_args"
-cmd="snakemake --jobs 42 $args $cluster_args --forceall"
-
-echo $cmd; $cmd
+echo "running snakemake ..."
+cmd="snakemake --jobs 42 $args $cluster_args"
+echo $cmd
+$cmd
 
